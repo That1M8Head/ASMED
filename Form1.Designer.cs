6138809eba8c6cd00ce5bfa08cce3b7f1a4c1d26
Arsalan Kazmi <kaz18001436@student.westherts.ac.uk>
2022-04-19 12:11:58 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace A2
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textArea = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.openButton = new System.Windows.Forms.Button();
+            this.newButton = new System.Windows.Forms.Button();
+            this.fontButton = new System.Windows.Forms.Button();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.helpButton = new System.Windows.Forms.Button();
+            this.echoArea = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textArea
+            // 
+            this.textArea.AcceptsReturn = true;
+            this.textArea.AcceptsTab = true;
+            this.textArea.AllowDrop = true;
+            this.textArea.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textArea.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(69)))), ((int)(((byte)(90)))));
+            this.textArea.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textArea.Font = new System.Drawing.Font("Consolas", 11F);
+            this.textArea.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(242)))));
+            this.textArea.Location = new System.Drawing.Point(174, 0);
+            this.textArea.Margin = new System.Windows.Forms.Padding(12, 12, 12, 12);
+            this.textArea.Multiline = true;
+            this.textArea.Name = "textArea";
+            this.textArea.Size = new System.Drawing.Size(769, 410);
+            this.textArea.TabIndex = 4;
+            this.textArea.TextChanged += new System.EventHandler(this.textArea_TextChanged);
+            // 
+            // saveButton
+            // 
+            this.saveButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(69)))), ((int)(((byte)(90)))));
+            this.saveButton.FlatAppearance.BorderSize = 0;
+            this.saveButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.saveButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(242)))));
+            this.saveButton.Location = new System.Drawing.Point(14, 57);
+            this.saveButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(144, 36);
+            this.saveButton.TabIndex = 5;
+            this.saveButton.Text = "Save <C-s>";
+            this.saveButton.UseVisualStyleBackColor = false;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // openButton
+            // 
+            this.openButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(69)))), ((int)(((byte)(90)))));
+            this.openButton.FlatAppearance.BorderSize = 0;
+            this.openButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.openButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(242)))));
+            this.openButton.Location = new System.Drawing.Point(14, 99);
+            this.openButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(144, 36);
+            this.openButton.TabIndex = 6;
+            this.openButton.Text = "Open <C-f>";
+            this.openButton.UseVisualStyleBackColor = false;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // newButton
+            // 
+            this.newButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(69)))), ((int)(((byte)(90)))));
+            this.newButton.FlatAppearance.BorderSize = 0;
+            this.newButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.newButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(242)))));
+            this.newButton.Location = new System.Drawing.Point(14, 14);
+            this.newButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.newButton.Name = "newButton";
+            this.newButton.Size = new System.Drawing.Size(144, 36);
+            this.newButton.TabIndex = 5;
+            this.newButton.Text = "Clear <C-n>";
+            this.newButton.UseVisualStyleBackColor = false;
+            this.newButton.Click += new System.EventHandler(this.newButton_Click);
+            // 
+            // fontButton
+            // 
+            this.fontButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(69)))), ((int)(((byte)(90)))));
+            this.fontButton.FlatAppearance.BorderSize = 0;
+            this.fontButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.fontButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(242)))));
+            this.fontButton.Location = new System.Drawing.Point(14, 142);
+            this.fontButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.fontButton.Name = "fontButton";
+            this.fontButton.Size = new System.Drawing.Size(144, 36);
+            this.fontButton.TabIndex = 6;
+            this.fontButton.Text = "Font <C-m>";
+            this.fontButton.UseVisualStyleBackColor = false;
+            this.fontButton.Click += new System.EventHandler(this.fontButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.closeButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(69)))), ((int)(((byte)(90)))));
+            this.closeButton.FlatAppearance.BorderSize = 0;
+            this.closeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.closeButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(242)))));
+            this.closeButton.Location = new System.Drawing.Point(14, 364);
+            this.closeButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(144, 36);
+            this.closeButton.TabIndex = 6;
+            this.closeButton.Text = "Close <A-q>";
+            this.closeButton.UseVisualStyleBackColor = false;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // helpButton
+            // 
+            this.helpButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.helpButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(69)))), ((int)(((byte)(90)))));
+            this.helpButton.FlatAppearance.BorderSize = 0;
+            this.helpButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.helpButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(242)))));
+            this.helpButton.Location = new System.Drawing.Point(14, 322);
+            this.helpButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.helpButton.Name = "helpButton";
+            this.helpButton.Size = new System.Drawing.Size(144, 36);
+            this.helpButton.TabIndex = 7;
+            this.helpButton.Text = "Help <C-h>";
+            this.helpButton.UseVisualStyleBackColor = false;
+            this.helpButton.Click += new System.EventHandler(this.helpButton_Click);
+            // 
+            // echoArea
+            // 
+            this.echoArea.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(69)))), ((int)(((byte)(90)))));
+            this.echoArea.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.echoArea.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(242)))));
+            this.echoArea.Location = new System.Drawing.Point(0, 412);
+            this.echoArea.Margin = new System.Windows.Forms.Padding(12);
+            this.echoArea.Name = "echoArea";
+            this.echoArea.Size = new System.Drawing.Size(943, 28);
+            this.echoArea.TabIndex = 9;
+            this.echoArea.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(34)))), ((int)(((byte)(45)))));
+            this.ClientSize = new System.Drawing.Size(943, 440);
+            this.Controls.Add(this.echoArea);
+            this.Controls.Add(this.helpButton);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.fontButton);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.newButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.textArea);
+            this.Font = new System.Drawing.Font("Consolas", 10F);
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MinimumSize = new System.Drawing.Size(629, 349);
+            this.Name = "MainWindow";
+            this.Text = "ASMED";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainWindow_MouseDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textArea;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Button newButton;
+        private System.Windows.Forms.Button fontButton;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Button helpButton;
+        private System.Windows.Forms.Label echoArea;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Forms;
+
+namespace A2
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void MainWindow_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        void fontSelect()
+        {
+            fontDialog1.Font = textArea.Font;
+            if (fontDialog1.ShowDialog() != DialogResult.Cancel)
+            {
+                if (fontDialog1.Font.Size >= 12)
+                {
+                    textArea.Font = fontDialog1.Font;
+                    echoArea.Text = "Chosen font too big, not updating UI";
+                }
+                else
+                {
+                    foreach (Control c in Controls)
+                    {
+                        c.Font = fontDialog1.Font;
+                    }
+                }
+            }
+        }
+
+        private void fontButton_Click(object sender, EventArgs e)
+        {
+            fontSelect();
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == (Keys.Control | Keys.M))
+            {
+                fontSelect();
+                return true;
+            }
+            else if (keyData == (Keys.Control | Keys.N))
+            {
+                textArea.Text = "";
+                return true;
+            }
+            else if (keyData == (Keys.Control | Keys.S))
+            {
+                if (textArea.Text == "")
+                {
+                    echoArea.Text = "Cannot save, text area is empty";
+                }
+                else
+                {
+                    saveFileDialog1.ShowDialog();
+                }
+                return true;
+            }
+            else if (keyData == (Keys.Control | Keys.F))
+            {
+                openFileDialog1.ShowDialog();
+                return true;
+            }
+            else if (keyData == (Keys.Control | Keys.H))
+            {
+                helpFunc();
+            }
+            else if (keyData == (Keys.Alt | Keys.Q))
+            {
+                Close();
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        void helpFunc()
+        {
+            textArea.Text = 
+                "ASMED (Another SMol EDitor) is a volatile text editor, meaning that there are no restrictions against unsaved work or bad files." +
+                Environment.NewLine +
+                Environment.NewLine +
+                "This is the text area, where you can write in."+
+                Environment.NewLine +
+                "The bar at the bottom is the echo area, where error messages will be printed." +
+                Environment.NewLine +
+                Environment.NewLine +
+                "Keyboard shortcuts" + 
+                Environment.NewLine + "====================" + 
+                Environment.NewLine + 
+                Environment.NewLine + 
+                "C means use the Control key, A means use the Alt key" + 
+                Environment.NewLine + 
+                Environment.NewLine + 
+                "<C-n> Clear the text area." + 
+                Environment.NewLine + 
+                "<C-s> Save the contents of the text area to a file." + 
+                Environment.NewLine + 
+                "<C-f> Open a file." + 
+                Environment.NewLine +
+                "<C-m> Change the font." + 
+                Environment.NewLine + 
+                "<C-h> Display this help." + 
+                Environment.NewLine + 
+                "<A-q> Exit.";
+        }
+
+        private void newButton_Click(object sender, EventArgs e)
+        {
+            textArea.Text = "";
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (textArea.Text == "")
+            {
+                echoArea.Text = "Cannot save, text area is empty";
+            }
+            else
+            {
+                saveFileDialog1.ShowDialog();
+            }
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            File.WriteAllText(saveFileDialog1.FileName, textArea.Text);
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            textArea.Text = File.ReadAllText(openFileDialog1.FileName);
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void helpButton_Click(object sender, EventArgs e)
+        {
+            helpFunc();
+        }
+
+        private void textArea_TextChanged(object sender, EventArgs e)
+        {
+            echoArea.Text = "";
+        }
+    }
+}
